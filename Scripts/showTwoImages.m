e528ef2c525b8963ae489962da914b1e10827dec
sankios <mattia.sanchioni.dev@gmail.com>
2018-11-26 16:17:53 +0100

    aggiunta descrizione funzione

--- Scripts/showTwoImages.m
+++ Scripts/showTwoImages.m
@@ -1,6 +1,7 @@
 function showTwoImages(fig1,fig2, name)
-%SHOWTWOIMAGES Summary of this function goes here
-%   Detailed explanation goes here
+%SHOWTWOIMAGES Display two images in a single figure named with \name
+%   SHOWTWOIMAGES(fig1, fig2, name) The two images in input \fig1 and \fig2 are displayed with a
+%   concatenation horizontaly in the same figure
 figure('Name', name);
 imshowpair(fig1, fig2, 'montage');
 end
